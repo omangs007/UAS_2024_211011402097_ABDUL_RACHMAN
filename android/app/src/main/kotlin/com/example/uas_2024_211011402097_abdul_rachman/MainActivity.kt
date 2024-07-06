da52bdd77d0e4c1b132ac4b182a5d0ee98e75b48
giovanno27 <145136624+giovanno27@users.noreply.github.com>
2024-07-06 14:09:21 +0700

    updated uas

--- android/app/src/main/kotlin/com/example/uas_2024_211011402097_abdul_rachman/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.uas_2024_211011402097_abdul_rachman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"uas_2024_211011401581_aldo", origin, size)) {
+  if (!window.Create(L"uas_2024_211011402097_abdul_rachman", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
